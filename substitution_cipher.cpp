5dfb64aa0d14e494ef1432196ed5564c8b57a79a
Swastik Banerjee <swastik_banerjee07@yahoo.co.in>
2019-06-02 13:29:43 +0530

    finds frequency of each letter in the ciphertext
    this code can also be used to break other ciphers like Affine ciphers,etc. by tallying the output frequencies of each letter in the ciphertext with the statistically found tabulated frequencies of most common letters in English alphabets

---
+++ substitution_cipher.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <climits>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+// author:Swastik Banerjee
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+
+#ifndef ONLINE_JUDGE
+  FILE *f1 = freopen("input.txt", "r", stdin);
+  FILE *f2 = freopen("output.txt", "w", stdout);
+#endif
+
+  string s;
+  cin >> s;
+  char A[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+              'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+  ll vx[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+  for (ll i = 0; i < s.size(); i++) {
+    vx[s[i] - 'A']++;
+  }
+  for (ll i = 0; i < 26; i++) {
+    cout << A[i] << " : " << vx[i] << endl;
+  }
+
+  /*for (ll i = 0; i < s.size(); i++) {
+    s[i] = char((11 * ((int)s[i] - 65) + 8) % 26 + 65 + 32);
+  }
+  cout << s;*/
+}
